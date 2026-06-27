5b690e7772e82c6879f2a714173372cff7d1964f
AlfredoLM940430 <alfredo.lm392@gmail.com>
2026-06-27 12:32:01 -0600

    Fix maps duplicados

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,23 +1,31 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // <-- Corregido: 'plugin-react' en vez de 'react-plugin'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
   optimizeDeps: {
-    exclude: ['maplibre-gl']
+    exclude: ['maplibre-gl'],
+    esbuildOptions: {
+      target: 'esnext',
+    }
   },
   esbuild: {
     keepNames: true,
     minifyIdentifiers: false,
+    supported: {
+      bigint: false
+    }
   },
   build: {
     commonjsOptions: {
       include: [/maplibre-gl/, /node_modules/]
+    },
+    rollupOptions: {
+      external: ['maplibre-gl'],
     },
     sourcemap: false
   }
